61beb000e29d2d1ce8d3a474af52837ca7d3dcd8
jkeychan <jkeychan@ibiblio.org>
2023-12-18 20:46:06 -0500

    modularized sections into functions

--- mc.sh
+++ mc.sh
@@ -2,20 +2,50 @@
 
 # Minecraft Server Startup Script
 
-# Define the path to the Java binary
-JAVA_BIN="/usr/bin/java"
+start_minecraft_server() {
+    local java_bin="/usr/bin/java"
+    local working_dir="/home/azureuser/MC"
+    local server_jar="/home/azureuser/MC/server.jar"
+    local screen_session_name="MC"
 
-# Define the working directory for the Minecraft server
-WORKING_DIR="/home/azureuser/MC"
+    # Check if Java binary exists
+    if [ ! -f "$java_bin" ]; then
+        echo "Error: Java binary not found at $java_bin"
+        exit 1
+    fi
 
-# Define the path to the Minecraft server JAR file
-SERVER_JAR="/home/azureuser/MC/server.jar"
+    # Check if server JAR exists
+    if [ ! -f "$server_jar" ]; then
+        echo "Error: Server JAR not found at $server_jar"
+        exit 1
+    fi
 
-# Define Java arguments
-JAVA_ARGS="-Xms6G -Xmx6G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1"
+    # Java arguments
+    local java_args=(
+        -Xms6G
+        -Xmx6G
+        -XX:+UseG1GC
+        -XX:+ParallelRefProcEnabled
+        -XX:MaxGCPauseMillis=200
+        -XX:+UnlockExperimentalVMOptions
+        -XX:+DisableExplicitGC
+        -XX:+AlwaysPreTouch
+        -XX:G1NewSizePercent=30
+        -XX:G1MaxNewSizePercent=40
+        -XX:G1HeapRegionSize=8M
+        -XX:G1ReservePercent=20
+        -XX:G1HeapWastePercent=5
+        -XX:G1MixedGCCountTarget=4
+        -XX:InitiatingHeapOccupancyPercent=15
+        -XX:G1MixedGCLiveThresholdPercent=90
+        -XX:G1RSetUpdatingPauseTimePercent=5
+        -XX:SurvivorRatio=32
+        -XX:+PerfDisableSharedMem
+        -XX:MaxTenuringThreshold=1
+    )
 
-# Define the log format (currently not effective for Minecraft logging)
-LOG_FORMAT="-Djava.util.logging.SimpleFormatter.format='[%1$tF-%1$tT%1$tz] [%4$s] %5$s %n'"
+    # Start the Minecraft server inside a screen session
+    /usr/bin/screen -dmS "$screen_session_name" /usr/bin/bash -c "cd $working_dir && exec $java_bin ${java_args[*]} -jar $server_jar --nogui"
+}
 
-# Start the Minecraft server inside a screen session
-/usr/bin/screen -dmS MC /usr/bin/bash -c "cd $WORKING_DIR && $JAVA_BIN $JAVA_ARGS $LOG_FORMAT -jar $SERVER_JAR --nogui"
+start_minecraft_server
