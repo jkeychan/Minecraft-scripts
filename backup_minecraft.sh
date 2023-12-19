6331ef5fc7d0dbf0180cf7a0b3144867676c8e5b
jkeychan <jkeychan@ibiblio.org>
2023-12-18 21:14:40 -0500

    refactored and updated

--- backup_minecraft.sh
+++ backup_minecraft.sh
@@ -2,28 +2,53 @@
 
 # Minecraft Backup Script
 
-# Define variables
-SERVER_DIR="/home/azureuser/MC" # Your Minecraft server directory
-WORLD_NAME="fartlord-1" # Name of the Minecraft world
-BACKUP_DIR="/home/azureuser/MC/backups" # Backup directory
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S") # Timestamp for backup file
+# Configuration
+SERVER_DIR="/home/azureuser/MC"
+WORLD_NAME="$YOURWORLDNAME"
+BACKUP_DIR="/home/azureuser/MC/backups"
+RETENTION_DAYS=8 # Number of days to keep backups
+LOG_FILE="/home/azureuser/MC/logs/minecraft_backup.log"
+
+# Create backup filename
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 BACKUP_FILE="$BACKUP_DIR/${WORLD_NAME}_backup_$TIMESTAMP.tar.gz"
 
 # Ensure backup directory exists
 mkdir -p "$BACKUP_DIR"
 
-# Stop the Minecraft server
-# Use your method to stop the server. Here's an example using systemd:
-sudo systemctl stop minecraft
+# Logging function
+log() {
+    echo "[$(date +"%Y-%m-%d %H:%M:%S")] $1" | tee -a "$LOG_FILE"
+}
 
-# Wait a bit to ensure the server has stopped
+# Stop the Minecraft server
+log "Stopping Minecraft server..."
+if ! sudo systemctl stop minecraft; then
+    log "Error stopping server"
+    exit 1
+fi
+
+# Wait for server to stop
 sleep 10
 
-# Create the backup
-tar -czvf "$BACKUP_FILE" -C "$SERVER_DIR" "$WORLD_NAME"
+# Backup
+log "Creating backup..."
+if tar -czvf "$BACKUP_FILE" -C "$SERVER_DIR" "$WORLD_NAME"; then
+    log "Backup created successfully"
+else
+    log "Error creating backup"
+    exit 1
+fi
 
-# Restart the Minecraft server
-sudo systemctl start minecraft
+# Start the Minecraft server
+log "Starting Minecraft server..."
+if ! sudo systemctl start minecraft; then
+    log "Error starting server"
+    exit 1
+fi
 
-# Optional: Delete old backups if you want to limit the number of backups
-find "$BACKUP_DIR" -name "${WORLD_NAME}_backup_*.tar.gz" -mtime +5 -delete
+# Delete old backups
+log "Deleting old backups..."
+find "$BACKUP_DIR" -name "${WORLD_NAME}_backup_*.tar.gz" -mtime +$RETENTION_DAYS -delete
+
+log "Backup process completed"
